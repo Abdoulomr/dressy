d45be66e6e09e55f367f6e6f16a570c8ac4f3b63
unknown <syabdoul100@gmail.com>
2023-06-11 14:21:15 +0000

    login form styling update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -26,26 +26,32 @@ const Page = () => {
       </h1>
       <form
         onSubmit={handleLogin}
-        className="pt-8 w-96 flex flex-col gap-10 justify-center align-center bg-white p-10 mt-4 shadow-md rounded-md"
+        className="py-12 w-11/12 max-w-lg flex flex-col gap-10 justify-center align-center bg-white px-10 mt-4 shadow-md rounded-md"
       >
-        <input
-          className="px-4 py-3 outline-none w-100 bg-slate-50 border-l-[2px] rounded-r-lg border-slate-950  focus:bg-slate-100 "
-          type="text"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="current-password"
-          placeholder="Adresse email"
-        />
-        <input
-          className="px-4 py-3 outline-none w-100 bg-slate-50 border-l-[2px] rounded-r-lg border-slate-950  focus:bg-slate-100  "
-          type="password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Mot de passe"
-          autoComplete="current-password"
-        />
+        <section className="flex flex-col gap-2">
+          <span>Adresse Email</span>
+          <input
+            className="px-4 py-3 outline-none w-100 bg-slate-50 rounded-lg  focus:bg-slate-100 "
+            type="text"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="current-password"
+            // placeholder=""
+          />
+        </section>
+        <section className="flex flex-col gap-2">
+          <span>Mot de passe</span>
+          <input
+            className="px-4 py-3 outline-none w-100 bg-slate-50 rounded-lg   focus:bg-slate-100  "
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            // placeholder=""
+            autoComplete="current-password"
+          />
+        </section>
 
         <button className="bg-slate-800 py-3 text-white hover:bg-slate-700 rounded-md">
           Se connecter
